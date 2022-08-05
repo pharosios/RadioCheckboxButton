9340ee7794e22abdf7ad7c832837526a424ed764
Ramy Rizkalla <ramy.rizkalla@pharos-solutions.de>
2022-08-05 14:44:52 +0200

    Support Carthage

---
+++ MBRadioButton/RadioCheckboxButton.h
@@ -0,0 +1,18 @@
+//
+//  RadioCheckboxButton.h
+//  RadioCheckboxButton
+//
+//  Created by Ramy Rizkalla on 05/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RadioCheckboxButton.
+FOUNDATION_EXPORT double RadioCheckboxButtonVersionNumber;
+
+//! Project version string for RadioCheckboxButton.
+FOUNDATION_EXPORT const unsigned char RadioCheckboxButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RadioCheckboxButton/PublicHeader.h>
+
+
